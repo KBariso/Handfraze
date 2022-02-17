4791ef3cc9b924d7f9baaf2b4b8846f7d652ff42
kielbariso@gmail.com <kielbariso@gmail.com>
2022-02-17 11:06:58 -0600

    post works now for phrase

--- app/forms/phrases.py
+++ app/forms/phrases.py
@@ -7,5 +7,5 @@ class NewPhrase(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
     description = StringField("Description", validators=[DataRequired()])
     media_url = StringField("media_url")
-    category_id = SelectField("category")
+    category_id = SelectField("category", choices=[(1, "Basic Phrase"),(2, "Greeting"), (3, "Food"), (4, "Shopping"), (5, "Location")])
     submit = SubmitField("Submit")
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,12 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import phraseReducer from './phrases';
-// import categoryReducer from './categories';
+import categoryReducer from './categories';
 
 const rootReducer = combineReducers({
   session,
   phrases: phraseReducer,
-  // categories: categoryReducer
+  categories: categoryReducer
 });
 
 
