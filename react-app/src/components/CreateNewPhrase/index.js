2cb1808d05b4ea0146bf7a170634ee5c2249f141
kielbariso@gmail.com <kielbariso@gmail.com>
2022-02-28 11:21:13 -0600

    revert

--- react-app/src/components/CreateNewPhrase/index.js
+++ react-app/src/components/CreateNewPhrase/index.js
@@ -68,12 +68,7 @@ const CreateNewPhrase = () => {
       setErrors(["Please select a category"]);
     } else if (!media.length) {
       setMedia("https://play-lh.googleusercontent.com/4UPSnZVYh4pEeD85XXUAi3Lhdfuw54rGD2kcy--BA8t86Zuua1NpLQxUeVS7QzUZ91g");
-    } else if (media.length) {
-      const embedRex = /embed/;
-      if (!embedRex.test(media)) {
-        setErrors(["Please enter a valid Embeded URL"]);
-      }
-    }  else {
+    } else {
       setErrors([]);
 
     let createdPhrase = await dispatch(createPhrase(payload))
