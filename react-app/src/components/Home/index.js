0be20ab15dd7490ce7b419e45d1555313d79e575
kielbariso@gmail.com <kielbariso@gmail.com>
2022-04-28 19:00:11 -0500

    welcoming

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -1,16 +1,15 @@
-import logo from './images/ASL-cover-image.jpg'
 import './Home.css'
 
 const Home = () => {
     return (
         <>
                     <div className="HomeBigPicContainer">
-                        <img
-                            src={logo}
-                            className="HomeBigPic"
-                            alt="website center picture"
-                        />
-                        <p className='test'>hello</p>
+                        <div className="HomeBigPic">
+                            <div className="WelcomeHandfraze">
+                                <h1>WELCOME TO HANDFRAZE</h1>
+                                <h3>A simple guide to American Sign Language phrases</h3>
+                            </div>
+                        </div>
                     </div>
         </>
     )
